9ef1e4740dbf34fe61d4058f07ef984fdb790b9d
Kamil Stadryniak <kamil.stadryniak@ttpsc.pl>
2020-05-18 10:50:49 +0200

    Added ORM additional projects and types.

---
+++ UnitTestingExamplesCore_KarieraIT.Domain/Entities/Bar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestingExamplesCore_KarieraIT.Domain.Entities
+{
+    public class Bar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Foo Foo { get; set; }
+    }
+}
---
+++ UnitTestingExamplesCore_KarieraIT.Domain/Entities/Foo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestingExamplesCore_KarieraIT.Domain.Entities
+{
+    public class Foo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Bar> Bars { get; set; }
+    }
+}
